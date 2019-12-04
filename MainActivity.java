8f6673674e1a78842cd32c60b847079e58ffda49
Tarik Salay <tariksalay@gmail.com>
2019-12-03 22:54:42 -0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,39 +1,66 @@
-package com.example.umpirelab;
+package com.example.umpirebuddy;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private int countBall = 0;
     private int countStrike = 0;
-    private TextView ballView;
-    private TextView strikeView;
+    TextView BallView;
+    TextView StrikeView;
+
+    private void alertView(String message) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(message)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BallView = (TextView) findViewById(R.id.BallView);
+        StrikeView = (TextView) findViewById(R.id.StrikeView);
     }
 
     public void ballClicked(View view) {
         countBall++;
-        if(countBall < 5){
-            ballView.setText(String.valueOf(countBall));
+        if (countBall < 4) {
+            BallView.setText("Ball: " + countBall);
 
-        }else{
-            ballView.setText("Walk!");
+        } else {
+            alertView("Walk!!");
+            countBall = 0;
+            countStrike = 0;
+            BallView.setText("Ball: " + countBall);
+            StrikeView.setText("Strike: " + countStrike);
         }
     }
 
     public void strikeClicked(View view) {
         countStrike++;
-        if (countStrike < 4){
-            strikeView.setText(String.valueOf(countStrike));
-        }else{
-            strikeView.setText("Out!");
+        if (countStrike < 3) {
+            StrikeView.setText("Strike: " + countStrike);
+        } else {
+            alertView("Out!!");
+            countBall = 0;
+            countStrike = 0;
+            BallView.setText("Ball: " + countBall);
+            StrikeView.setText("Strike: " + countStrike);
+
+
         }
+
     }
 }
